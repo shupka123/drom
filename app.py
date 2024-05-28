9bfe2080ce56dab349fa9942d80eb0b654b6b8c2
shupka123 <167852173+shupka123@users.noreply.github.com>
2024-05-28 08:05:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,19 +32,19 @@ except Exception as _ex:
 @app.route('/users', methods=['POST', 'DELETE'])
 def users():
     if request.method == 'POST':
-        response = requests.post('http://172.21.0.5:5000/users', json=request.json)
+        response = requests.post('http://172.21.0.4:5000/users', json=request.json)
         return jsonify(response.json()), response.status_code
     elif request.method == 'DELETE':
-        response = requests.delete('http://172.21.0.5:5000/users', json=request.json)
+        response = requests.delete('http://172.21.0.4:5000/users', json=request.json)
         return jsonify(response.json()), response.status_code
 
 @app.route('/car_area', methods=['GET', 'POST'])
 def cars():
     if request.method == 'GET':
-        response = requests.get('http://172.21.0.4:5000/car_area')
+        response = requests.get('http://172.21.0.5:5000/car_area', json=request.json)
         return jsonify(response.json()), response.status_code
     elif request.method == 'POST':
-        response = requests.post('http://172.21.0.4:5000/car_area', json=request.json)
+        response = requests.post('http://172.21.0.5:5000/car_area', json=request.json)
         return jsonify(response.json()), response.status_code
 
 @app.route('/car_area/<int:car_id>', methods=['GET', 'PUT', 'DELETE'])
