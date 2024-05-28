9304c3204bcc41ee6bec724a604ed0bf15d3e4d1
shupka123 <167852173+shupka123@users.noreply.github.com>
2024-05-28 07:14:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ def users():
 @app.route('/car_area', methods=['GET', 'POST'])
 def cars():
     if request.method == 'GET':
-        response = requests.get('http://172.21.0.4:5001/car_area')
+        response = requests.get('http://172.21.0.4:5000/car_area')
         return jsonify(response.json()), response.status_code
     elif request.method == 'POST':
-        response = requests.post('http://172.21.0.4:5001/car_area', json=request.json)
+        response = requests.post('http://172.21.0.4:5000/car_area', json=request.json)
         return jsonify(response.json()), response.status_code
 
 @app.route('/car_area/<int:car_id>', methods=['GET', 'PUT', 'DELETE'])
